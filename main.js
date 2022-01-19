f5ebbe77791f4ea1c55fa7f5397d3dfc366fa97d
Francesco Maestrelli <framae26@gmail.com>
2022-01-19 15:25:42 +0100

    impostaz. di base

---
+++ main.js
@@ -0,0 +1,25 @@
+/* Calcolo anno attuale */
+
+// const annoNascita = 1988;
+// let eta = prompt("Quanti anni hai?");
+// eta = parseInt(eta);
+// let annoAttuale = annoNascita + eta;
+// document.getElementById('annoAttuale').innerHTML = annoAttuale;
+
+
+
+//camel case
+// let annoDiNascita;
+
+// richiesta nome
+
+
+
+
+// richiesta cognome
+
+
+
+
+
+// richiesta colore preferito
